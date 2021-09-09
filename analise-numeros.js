5ebfaf2b66234a43f7741484164e9ce64e6d4ac4
pldnz <paulosouzadiniz@hotmail.com>
2021-09-08 21:24:58 -0300

    desenvolvimento da solução para o desafio analise de números

---
+++ analise-numeros.js
@@ -0,0 +1,14 @@
+numero = [1,4,8,6,3];
+
+pares = numero.filter();
+
+// impares = numero.filter(                   );
+
+// positivos = numero.filter(                    );
+// negativos = numero.filter(                                 );
+
+print(pares.length + " valor(es) par(es)");
+// print(impares.length + " valor(es) impar(es)");
+
+// print(positivos.length + " valor(es) positivo(s)");
+// print(negativos.length + " valor(es) negativo(s)");
